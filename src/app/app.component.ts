70bc31269e51d9ae6b8b3b642f19e90ba12be66c
Thinh Pham <thinh.pham@impartner.com>
2023-05-03 20:42:04 +0700

    feat: edit raw file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'email-content-tool';
@@ -16,28 +16,38 @@ export class AppComponent implements OnInit {
   public isProcess: boolean = false;
   public editQuery: boolean = false;
   public isQueryInset: boolean = true;
+  public isEditingRawFile: boolean = false;
 
-  constructor(private httpClient: HttpClient, private sanitizer: DomSanitizer) {}
+  constructor(
+    private httpClient: HttpClient,
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
-    this.httpClient.get('assets/emails_emailscontent.sql', { responseType: 'text' }).subscribe(res => {
-      if (res) {
-        this.queryText = res;
-      }
-    }
-    )
+    this.httpClient
+      .get('assets/emails_emailscontent.sql', { responseType: 'text' })
+      .subscribe((res) => {
+        if (res) {
+          this.queryText = res;
+        }
+      });
   }
 
   public rePlaceKeyCode(): void {
-    let result = this.queryText
-    this.properties.forEach(property => {
-      const keys = Object.keys(property).filter(key => key !== 'propertyName');
-      keys.forEach(key => {
+    let result = this.queryText;
+    this.properties.forEach((property) => {
+      const keys = Object.keys(property).filter(
+        (key) => key !== 'propertyName'
+      );
+      keys.forEach((key) => {
         if (key.trim().length >= 0) {
-          result = result.replaceAll(`##${property.propertyName + key}##`, property[key] ?? '');
+          result = result.replaceAll(
+            `##${property.propertyName + key}##`,
+            property[key] ?? ''
+          );
         }
-      })
-    })
+      });
+    });
     this.queryShow = result;
   }
 
@@ -47,7 +57,6 @@ export class AppComponent implements OnInit {
       this.convertAllInsertToUpdate();
     }
     this.isProcess = true;
-
   }
 
   public onInputChange(event: Event, property: IProperties, tap: string): void {
@@ -59,7 +68,7 @@ export class AppComponent implements OnInit {
 
   public copyQuery(): void {
     navigator.clipboard.writeText(this.queryShow);
-    alert('copied !!')
+    alert('copied !!');
   }
 
   public back() {
@@ -69,12 +78,16 @@ export class AppComponent implements OnInit {
 
   public handleQueyChange(event: Event): void {
     if (event.target) {
-      this.queryText = (event.target as HTMLInputElement).value ?? '';
+      this.queryShow = (event.target as HTMLInputElement).value ?? '';
     }
   }
 
   public closeEditQuery(): void {
     this.editQuery = false;
+    if (this.isEditingRawFile) {
+      this.isEditingRawFile = false;
+      this.queryText = this.queryShow;
+    }
   }
 
   public openEditQuery(): void {
@@ -82,33 +95,50 @@ export class AppComponent implements OnInit {
   }
 
   public convertAllInsertToUpdate(): void {
-    const tables: string[] = this.queryShow.split('INSERT INTO').map(value => 'INSERT INTO' + value);
+    const tables: string[] = this.queryShow
+      .split('INSERT INTO')
+      .map((value) => 'INSERT INTO' + value);
     tables.shift();
     this.queryShow = '';
-    tables.forEach(value => {
-      this.queryShow += this.convertInsertToUpdate(value)
-    })
+    tables.forEach((value) => {
+      this.queryShow += this.convertInsertToUpdate(value);
+    });
   }
 
   public convertInsertToUpdate(insertQuery: string): string {
-    const tableName = '`' + insertQuery.match(/(?<=(insert\sinto\s[`'"]))\w+(?=['"`])/gi)?.[0] + '`';
+    const tableName =
+      '`' +
+      insertQuery.match(/(?<=(insert\sinto\s[`'"]))\w+(?=['"`])/gi)?.[0] +
+      '`';
 
     const columnsStartIndex = insertQuery.indexOf('(') + 1;
-    const columns = insertQuery.slice(columnsStartIndex, insertQuery.indexOf(')')).split(',');
+    const columns = insertQuery
+      .slice(columnsStartIndex, insertQuery.indexOf(')'))
+      .split(',');
     const trimmedColumns = columns.map((col) => col.trim());
     trimmedColumns.shift();
 
     const valuesStartIndex = insertQuery.indexOf('VALUES') + 7;
     const valuesEndIndex = insertQuery.lastIndexOf(';');
-    const valuesStr = insertQuery.slice(valuesStartIndex, valuesEndIndex).trim();
-    const valuesList = valuesStr.slice(1, -1).split(/(?<=\d+\s*),(?=\s*\d+)|(?<=\w*\s*')\s*,\s*(?='\s*\w*)|(?<=\d+\s*),\s*(?='\s*\w*)|(?<=\w*\s*')\s*,(?=\s*\d+)/gi);
+    const valuesStr = insertQuery
+      .slice(valuesStartIndex, valuesEndIndex)
+      .trim();
+    const valuesList = valuesStr
+      .slice(1, -1)
+      .split(
+        /(?<=\d+\s*),(?=\s*\d+)|(?<=\w*\s*')\s*,\s*(?='\s*\w*)|(?<=\d+\s*),\s*(?='\s*\w*)|(?<=\w*\s*')\s*,(?=\s*\d+)/gi
+      );
     valuesList.shift();
 
     const trimmedValues = valuesList.map((val) => val.trim());
-    const setClauses = trimmedColumns.map((col, i) => `${col} = ${trimmedValues[i]}`).join(', ');
+    const setClauses = trimmedColumns
+      .map((col, i) => `${col} = ${trimmedValues[i]}`)
+      .join(', ');
     let updateQuery = `UPDATE ${tableName} SET ${setClauses}`;
 
-    const indexTrigger = trimmedColumns.findIndex(value => value === '`trigger`');
+    const indexTrigger = trimmedColumns.findIndex(
+      (value) => value === '`trigger`'
+    );
     const whereClauses = '`trigger`' + ' = ' + trimmedValues[indexTrigger];
     updateQuery += ` WHERE ${whereClauses};`;
 
@@ -116,16 +146,27 @@ export class AppComponent implements OnInit {
   }
 
   public changeQueryShow(type: 'inset' | 'update'): void {
-    if (type === 'inset' && !this.isQueryInset) {
+    if (this.isEditingRawFile) {
+      this.isEditingRawFile = false;
+      this.queryText = this.queryShow;
+    }
+
+    if (type === 'inset') {
       this.isQueryInset = true;
       this.rePlaceKeyCode();
-    } else if (this.isQueryInset) {
+    } else if (type === 'update') {
       this.isQueryInset = false;
+      this.rePlaceKeyCode();
       this.convertAllInsertToUpdate();
     }
   }
 
-  sanitize(url: string) {
+  public editRawFile() {
+    this.isEditingRawFile = true;
+    this.queryShow = this.queryText;
+  }
+
+  public sanitize(url: string) {
     return this.sanitizer.bypassSecurityTrustUrl(url);
   }
 }
