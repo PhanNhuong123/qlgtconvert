2d333e89f9813687f48ee740c639ec9e5e4c5359
Thinh Pham <thinh.pham@impartner.com>
2023-05-05 16:43:36 +0700

    refactor code

---
+++ src/app/store/global.store.ts
@@ -0,0 +1,70 @@
+import { ComponentStore } from '@ngrx/component-store';
+import { IProperty, ITemplate } from "../app.constant";
+import { Injectable } from '@angular/core';
+
+const state = {
+  properties: [
+    {
+      propertyName: 'logo',
+      url: '',
+      height: 'unset',
+      width: '200'
+    }
+  ] as IProperty[],
+
+  queryText: '' as string,
+  queryShow: '' as string,
+  isProcess: false as boolean,
+  editQuery: false as boolean,
+  isQueryInsert: false as boolean,
+  isEditingRawFile: false as boolean,
+  isShowTemplate: false as boolean,
+  errorText: 'Error to convert raw file !' as string,
+  emailTemplates: [] as ITemplate[],
+  emailTemplateIndex: 0 as number
+}
+
+type State = typeof state;
+
+@Injectable()
+export class GlobalStore extends ComponentStore<State> {
+  constructor() {
+    super(state);
+  }
+
+  get properties(): IProperty[] { return this.get().properties; }
+
+  get queryText(): string { return this.get().queryText; }
+  public updateQueryText(newText: string) { this.patchState({ queryText: newText }); }
+
+  get queryShow(): string { return this.get().queryShow; }
+  public updateQueryShow(newText: string) { this.patchState({ queryShow: newText }); }
+
+  get isProcess(): boolean { return this.get().isProcess; }
+  updateIsProcess(value: boolean) { this.patchState({ isProcess: value }); }
+
+  get editQuery(): boolean { return this.get().editQuery; }
+  updateEditQuery(value: boolean) { this.patchState({ editQuery: value }); }
+
+  get isQueryInsert(): boolean { return this.get().isQueryInsert; }
+  updateQueryInsert(value: boolean) { this.patchState({ isQueryInsert: value });}
+
+  get isEditingRawFile(): boolean { return this.get().isEditingRawFile; }
+  updateEditingRawFile(value: boolean) { this.patchState({ isEditingRawFile: value }); }
+
+  get errorText(): string { return this.get().errorText; }
+
+  get emailTemplateIndex(): number { return this.get().emailTemplateIndex; }
+  updateEmailTemplateIndex(value: number) { this.patchState({ emailTemplateIndex: value }); }
+
+  get isShowTemplate(): boolean { return this.get().isShowTemplate; }
+  updateShowTemplate(value: boolean) { this.patchState({ isShowTemplate: value }); }
+
+  get emailTemplates(): ITemplate[] { return this.get().emailTemplates; }
+  updateEmailTemplates(newTemplate: ITemplate) {
+    this.patchState({
+      emailTemplates: [...this.emailTemplates, newTemplate]
+    })
+  }
+
+}
